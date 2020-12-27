ddf81f58da9ace2104ccb6a85513c12603b0fae2
zhadjah9559 <zhadjah@gmail.com>
2020-12-26 23:17:22 -0500

    Completed calculation

--- CalculateFibonacci/Program.cs
+++ CalculateFibonacci/Program.cs
@@ -6,7 +6,23 @@ namespace CalculateFibonacci
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int num1 = 0;
+            int num2 = 1;
+            int num3;
+
+            Console.WriteLine("How many numbers of the fibonacci series would you like to see?");
+            int count = Int32.Parse(Console.ReadLine());
+
+            //First number in the series
+            Console.Write(num1 + " " + num2);
+            
+            for(int i = 2; i < count; i++)
+            {
+                num3 = num1 + num2;
+                Console.Write(" " + num3);
+                num1 = num2;
+                num2 = num3;
+            }
         }
     }
 }
